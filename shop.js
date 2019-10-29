74d034ebc71d966060dc2e249730f61943d67462
huunhan2014 <ng.h.nhan2014@gmail.com>
2019-10-29 21:57:21 +0700

    upload folder Website T&N Shop

---
+++ shop.js
@@ -0,0 +1,32 @@
+function change1() {
+	document.getElementById('full').src = document.getElementById('mini1').src;
+}
+
+function change2() {
+	document.getElementById('full').src = document.getElementById('mini2').src;
+}
+
+function change3() {
+	document.getElementById('full').src = document.getElementById('mini3').src;
+}
+
+function addCart() {
+	alert("SẢN PHẨM NÀY ĐÃ ĐƯỢC THÊM VÀO GIỎ.");
+}
+
+function show() {
+	document.getElementById('dathang').style.display = "block";
+}
+
+function changeLike() {
+	document.getElementById('like').src = "../img/like-red.jpg";
+}
+function changeShare() {
+	document.getElementById('share').src = "../img/share-red.jpg";
+}
+function changeGP() {
+	document.getElementById('gp').src = "../img/google-plus-red.jpg";
+}
+function changeTw() {
+	document.getElementById('tw').src = "../img/tw-red.jpg";
+}
